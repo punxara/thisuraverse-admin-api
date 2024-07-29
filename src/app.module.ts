3446bfc28b363e24344f33f236fa5aed50a6a5b9
BPunsara <bpunsara@jayjaymills.lk>
2024-07-29 15:27:27 +0530

    db connection test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,32 +1,45 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { MovieModule } from './movie/movie.module';
-import { SocialModule } from './social/social.module';
-import { Movie } from "./movie/movie";
-import { Social } from "./social/social";
-import { Role } from "./movie/role/role";
-import { Genre } from "./movie/genre/genre";
+import {Module} from "@nestjs/common";
+import {AppController} from "./app.controller";
+import {AppService} from "./app.service";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {MovieModule} from './movie/movie.module';
+import {SocialModule} from './social/social.module';
+import {Movie} from "./movie/movie";
+import {Social} from "./social/social";
+import {Role} from "./movie/role/role";
+import {Genre} from "./movie/genre/genre";
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: "postgres",
-      host: "localhost",
-      port: 5432,
-      username: "postgres",
-      password: "123",
-      database: "thisuraverse-master-db",
-      entities: [Movie, Social, Role, Genre],
-      synchronize: true,
-      logging: false
-    }),
-    MovieModule,
-    SocialModule
-  ],
-  controllers: [AppController],
-  providers: [AppService]
+    // imports: [
+    //   TypeOrmModule.forRoot({
+    //     type: "postgres",
+    //     host: "master-db-thisuraverse-data.e.aivencloud.com",
+    //     port: 22354,
+    //     username: "avnadmin",
+    //     password: "AVNS_QfLkMeWjVrWL9Ma6VIr",
+    //     database: "defaultdb",
+    //     entities: [Movie, Social, Role, Genre],
+    //     synchronize: true,
+    //     logging: false,
+    //     ssl: true,
+    //   }),
+    imports: [
+        TypeOrmModule.forRoot({
+            type: "postgres",
+            host: "172.20.10.12",
+            port: 5432,
+            username: "postgres",
+            password: "123",
+            database: "thisuraverse-master-db",
+            entities: [Movie, Social, Role, Genre],
+            synchronize: true,
+            logging: false
+        }),
+        MovieModule,
+        SocialModule
+    ],
+    controllers: [AppController],
+    providers: [AppService]
 })
 export class AppModule {
 }
