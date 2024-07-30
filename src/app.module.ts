d4cf569c66ba913af6ba690c139a1f4e8fc528de
BPunsara <bpunsara@jayjaymills.lk>
2024-07-30 10:04:05 +0530

    cloud database configuration completed

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,19 +8,35 @@ import {Movie} from "./movie/movie";
 import {Social} from "./social/social";
 import {Role} from "./movie/role/role";
 import {Genre} from "./movie/genre/genre";
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
     imports: [
+        ConfigModule.forRoot({ envFilePath: '.env' }),
+        // TypeOrmModule.forRoot({
+        //     type: "postgres",
+        //     host: "192.168.40.135",
+        //     port: 5432,
+        //     username: "postgres",
+        //     password: "123",
+        //     database: "thisuraverse-master-db",
+        //     entities: [Movie, Social, Role, Genre],
+        //     synchronize: true,
+        //     logging: false
+        // }),
         TypeOrmModule.forRoot({
             type: "postgres",
-            host: "192.168.40.135",
-            port: 5432,
-            username: "postgres",
-            password: "123",
-            database: "thisuraverse-master-db",
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
             entities: [Movie, Social, Role, Genre],
             synchronize: true,
-            logging: false
+            logging: false,
+            ssl: {
+                rejectUnauthorized: false
+            }
         }),
         MovieModule,
         SocialModule
